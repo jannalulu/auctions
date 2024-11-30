e0b98678d1cb33366fa95d181a12a68ee4f3c3ca
jannalulu <109004049+jannalulu@users.noreply.github.com>
2024-11-30 14:30:40 -0500

    add looping functionality

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# from auctions.english_auction import EnglishAuction
+from auctions.english_auction import EnglishAuction
 # from auctions.dutch_auction import DutchAuction
 from auctions.common_auction import CommonAuction
 from agents.buyer_agent import BuyerAgent
-
 from agents.auctioneer_agent import AuctioneerAgent
 import yaml
+import argparse
 
 def run_auction(config):
     buyers = [BuyerAgent(f"Buyer_{i}", buyer['attributes'], buyer['budget']) # Pass in buyer attributes from config
@@ -12,22 +12,32 @@ def run_auction(config):
     auctioneer = AuctioneerAgent("Auctioneer", config['auctioneer_attributes'])
     
     # Type of auction that I'm running
-    auction = CommonAuction( # auction = EnglishAuction( for English
+    auction = CommonAuction( # auction = EnglishAuction / CommonAuction / DutchAuction
         config['item'],
         config['starting_price'],
         buyers,
         auctioneer,
         config['price_increment'], #or config['price_decrement'] for Dutch Auctions
-        config['log_file'],
+        config['log_file']
         # config ['max_rounds'] # comment out for English and common auctions
     )
     
     results = auction.run()
     return results
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--loop', action='store_true', help='Run the auction 10 times')
+  args = parser.parse_args()
+  
   with open('config/config.yaml', 'r') as f:
       config = yaml.safe_load(f)
   
-  results = run_auction(config)
-  print(results)
+  if args.loop:
+    for i in range(10):
+      print(f"\nAuction {i+1}/10")
+      results = run_auction(config)
+      print(results)
+  else:
+    results = run_auction(config)
+    print(results)
